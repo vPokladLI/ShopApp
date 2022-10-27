8262656642958a001449184f2e343ef7f4172691
vPokladLI <viktor.p@li.solutions>
2022-10-27 12:30:54 +0300

    refactor orders from REST to SDK

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:firebase_database/firebase_database.dart';
+// import 'package:http/http.dart' as http;
 import 'package:shop_app/models/http_exception.dart';
 
 import './cart.dart';
+
+FirebaseDatabase database = FirebaseDatabase.instance;
 
 class OrderItem {
   final String id;
@@ -18,8 +21,7 @@ class OrderItem {
 }
 
 class Orders with ChangeNotifier {
-  static const _endpoint =
-      'flutter-shop-app-365508-default-rtdb.europe-west1.firebasedatabase.app';
+  final ref = database.ref();
   List<OrderItem> _orders = [];
 
   List<OrderItem> get items {
@@ -27,63 +29,62 @@ class Orders with ChangeNotifier {
   }
 
   Future fetchAndSetOrders() async {
-    final url = Uri.https(_endpoint, '/orders.json');
+    // final url = Uri.https(_endpoint, '/orders.json');
     try {
-      final response = await http.get(url);
-      if (response.statusCode == 200) {
-        final Map<String, dynamic> decodedResponse = jsonDecode(response.body);
-        if (decodedResponse.isNotEmpty) {
-          List<OrderItem> loadedOrders = [];
-          decodedResponse.forEach((orderId, order) {
-            loadedOrders.add(OrderItem(
-                id: orderId,
-                amount: order['amount'],
-                dateTime: DateTime.parse(order['dateTime']),
-                products: (order['products'] as List<dynamic>)
-                    .map((e) => CartItem(
-                        id: e['id'],
-                        price: e['price'],
-                        quantity: e['quantity'],
-                        title: e['title']))
-                    .toList()));
-          });
-          _orders = loadedOrders.reversed.toList();
-        }
+      final snapshot = await ref.child('/orders').get();
+      if (snapshot.value == null) {
+        return;
       }
+      final extractedData = snapshot.value as Map<dynamic, dynamic>;
+
+      List<OrderItem> loadedOrders = [];
+      extractedData.forEach((orderId, order) {
+        loadedOrders.add(OrderItem(
+            id: orderId,
+            amount: order['amount'],
+            dateTime: DateTime.parse(order['dateTime']),
+            products: (order['products'] as List<dynamic>)
+                .map((e) => CartItem(
+                    id: e['id'],
+                    price: e['price'],
+                    quantity: e['quantity'],
+                    title: e['title']))
+                .toList()));
+      });
+      _orders = loadedOrders.reversed.toList();
     } catch (e) {
       rethrow;
     }
   }
 
   Future addOrder(double amount, List<CartItem> products) async {
-    final url = Uri.https(_endpoint, '/orders.json');
     final timeStamp = DateTime.now();
+    DatabaseReference ordersRef = database.ref('/orders');
+    DatabaseReference newOrder = ordersRef.push();
+    String? orderId = ordersRef.push().key;
 
-    final response = await http.post(url,
-        body: json.encode({
-          'amount': amount,
-          'products': products
-              .map((el) => {
-                    'id': el.id,
-                    'title': el.title,
-                    'quantity': el.quantity,
-                    'price': el.price,
-                  })
-              .toList(),
-          'dateTime': timeStamp.toIso8601String(),
-        }));
-
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> decodedResponse = jsonDecode(response.body);
+    try {
+      await newOrder.set({
+        'amount': amount,
+        'products': products
+            .map((el) => {
+                  'id': el.id,
+                  'title': el.title,
+                  'quantity': el.quantity,
+                  'price': el.price,
+                })
+            .toList(),
+        'dateTime': timeStamp.toIso8601String()
+      });
       _orders.insert(
           0,
           OrderItem(
               amount: amount,
               products: products,
               dateTime: timeStamp,
-              id: decodedResponse['name']));
+              id: orderId!));
       notifyListeners();
-    } else {
+    } catch (e) {
       throw HttpException('Failed to make order... Try again later');
     }
   }
